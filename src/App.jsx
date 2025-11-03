9c078f6ee0965dc202e2ceecbac72d3537ea0049
Linnea1 <linnea.a.14@gmail.com>
2025-11-03 11:38:15 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './styles/pages.css';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/Cakery">
+    <BrowserRouter>
       <div className="app">
         <Navbar />
         <main>
